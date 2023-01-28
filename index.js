0e2d67647cd5e443a1086bba6d4dd96d40f7bb26
cameliabenavides10 <cameliabenavides@gmail.com>
2023-01-28 16:41:40 -0600

    added more funcitonality to creating html template

--- index.js
+++ index.js
@@ -56,6 +56,9 @@ function startApp() {
             .then((data) => {
                 const manager = new Manager(data.managerName, data.managerId, data.managerEmail, data.managerOffice);
                 theTeam.push(manager);
+                console.log(manager);
+                console.log(manager.getRole)
+                console.log(manager.email)
                 intTeam();
             });
 
@@ -126,98 +129,85 @@ function startApp() {
     }
     intTeam();
 
-    function makeHTML() {
-        fs.writeFileSync("profile.html", generateTemplate(theTeam), (err) =>
-            err ? console.log(err) : console.log('')
-        )
-    };
+    // function makeHTML() {
+    //     fs.writeFileSync("profile.html", generateTemplate(theTeam), (err) =>
+    //         err ? console.log(err) : console.log('')
+    //     )
+    // };
 
-    function generateTemplate() {
-        // var managerNameGrab = document.getElementById("#change-manager");
-        // var managerRole = documenet.getElementById("#manager-role");
-        // var managerIdGrab = document.getElementById("#manager-id");
-        // var managerEmailGrab = documenet.getElementById("#manager-email");
-        // var managerofficeGrab = documenet.getElementById("#manager-office");
+    // function generateTemplate() {
 
-        // managerNameGrab.innerHTML = Manager.employeeName;
-        // managerRole.innerHTML = Manager.getRole;
-        // managerIdGrab = Manager.id;
-        // managerEmailGrab = Manager.email;
-        // managerofficeGrab = Manager.getOfficeNumber;
-        
+    //     `<!DOCTYPE html>
+    //     <html lang="en">
+    //     <head>
+    //         <meta charset="UTF-8" />
+    //         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    //         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    //         <title>My Team</title>
+    //         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    //             integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    //         <link rel="stylesheet" href="style.css">
+    //         <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    //     </head>
+    //     <body>
+    //         <div class="container-fluid">
+    //             <div class="row">
+    //                 <div class="col-12 jumbotron mb-3 team-heading bg-danger">
+    //                     <h1 class="text-center text-white">My Team</h1>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //         <div class="container">
+    //             <div class="row">
+    //                 <div class="row team-area col-12 d-flex justify-content-center">`
 
-
-        `<!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-            <title>My Team</title>
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-            <link rel="stylesheet" href="style.css">
-            <script src="https://kit.fontawesome.com/c502137733.js"></script>
-        </head>
-        <body>
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-12 jumbotron mb-3 team-heading bg-danger">
-                        <h1 class="text-center text-white">My Team</h1>
-                    </div>
-                </div>
-            </div>
-            <div class="container">
-                <div class="row">
-                    <div class="row team-area col-12 d-flex justify-content-center">`
-
-
-        if (data.role === "Manager") {
-            `<div class="card employee-card">
-            <div class="card-header bg-primary text-white">
-                <h2 class="card-title" id="change-manager">Less Nessman</h2>
-                <h3 class="card-title" id="manager-role"><i class="fas fa-mug-hot mr-2"></i>Manager</h3>
-            </div>
-            <div class="card-body">
-                <ul class="list-group">
-                    <li class="list-group-item" id="manager-id">ID: 86525</li>
-                    <li class="list-group-item" id="manager-email">Email: <a href="mailto:bigboss@gmail.com">bigboss@gmail.com</a></li>
-                    <li class="list-group-item" id="manager-office">Office number: 512-459-2222</li>
-                </ul>
-            </div>
-        </div>`
-        } else if (data.role === "Engineer") {
-            `<div class="card employee-card">
-            <div class="card-header bg-primary text-white">
-                <h2 class="card-title">Tits Maghee</h2>
-                <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>Intern</h3>
-            </div>
-            <div class="card-body">
-                <ul class="list-group">
-                    <li class="list-group-item">ID: 8008</li>
-                    <li class="list-group-item">Email: <a href="mailto:tits@gmail.com">tits@gmail.com</a></li>
-                    <li class="list-group-item">School: TCU</li>
-                </ul>
-            </div>
-        </div>`
-        } else if (data.role === "Intern") {
-            `<div class="card employee-card">
-        <div class="card-header bg-primary text-white">
-            <h2 class="card-title">Tits Maghee</h2>
-            <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>Intern</h3>
-        </div>
-        <div class="card-body">
-            <ul class="list-group">
-                <li class="list-group-item">ID: 8008</li>
-                <li class="list-group-item">Email: <a href="mailto:tits@gmail.com">tits@gmail.com</a></li>
-                <li class="list-group-item">School: TCU</li>
-            </ul>
-        </div>
-    </div>`
-        }
+    //         if (employee.getRole === "Manager"){
+               
+    //                 `<div class="card employee-card">
+    //                 <div class="card-header bg-primary text-white">
+    //                     <h2 class="card-title" id="change-manager">${manager.employeeName}</h2>
+    //                     <h3 class="card-title" id="manager-role"><i class="fas fa-mug-hot mr-2"></i>Manager</h3>
+    //                 </div>
+    //                 <div class="card-body">
+    //                     <ul class="list-group">
+    //                         <li class="list-group-item" id="manager-id">${manager.id}</li>
+    //                         <li class="list-group-item" id="manager-email">Email: <a href="mailto:bigboss@gmail.com">bigboss@gmail.com</a></li>
+    //                         <li class="list-group-item" id="manager-office">Office number: 512-459-2222</li>
+    //                     </ul>
+    //                 </div>
+    //             </div>`
+    //             } else if(data.role === "Engineer") {
+    //         `<div class="card employee-card">
+    //         <div class="card-header bg-primary text-white">
+    //             <h2 class="card-title">Tits Maghee</h2>
+    //             <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>Intern</h3>
+    //         </div>
+    //         <div class="card-body">
+    //             <ul class="list-group">
+    //                 <li class="list-group-item">ID: 8008</li>
+    //                 <li class="list-group-item">Email: <a href="mailto:tits@gmail.com">tits@gmail.com</a></li>
+    //                 <li class="list-group-item">School: TCU</li>
+    //             </ul>
+    //         </div>
+    //     </div>`
+    //     } else if (data.role === "Intern") {
+    //         `<div class="card employee-card">
+    //     <div class="card-header bg-primary text-white">
+    //         <h2 class="card-title">Tits Maghee</h2>
+    //         <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>Intern</h3>
+    //     </div>
+    //     <div class="card-body">
+    //         <ul class="list-group">
+    //             <li class="list-group-item">ID: 8008</li>
+    //             <li class="list-group-item">Email: <a href="mailto:tits@gmail.com">tits@gmail.com</a></li>
+    //             <li class="list-group-item">School: TCU</li>
+    //         </ul>
+    //     </div>
+    // </div>`
+    //     }
 
 
-    }
+    // }
 
 
 
